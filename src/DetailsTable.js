efb4298db64cdad949f173f7352121c5042ff867
amnahkhatun <amna.khatun1605@gmail.com>
2022-10-28 16:00:23 +0530

    added delete functionality

--- src/DetailsTable.js
+++ src/DetailsTable.js
@@ -9,6 +9,9 @@ function DetailsTable() {
 
   const [data, setData] = useState([])
   const [query, setQuery] = useState('')
+  const [name, setName] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
 
   const getData = async () => {
     const data = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
@@ -17,9 +20,21 @@ function DetailsTable() {
     console.log(resp)
   }
 
+  const editItem = (id) => {
+    const specificItem = data.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
   useEffect(() => {
     getData()
   }, [])
+
+  const deleteItem = (id) => {
+    const updatedItems = data.filter((item) => item.id !== id)
+    setData(updatedItems)
+  }
 
   return (
     <Container>
@@ -49,7 +64,7 @@ function DetailsTable() {
         </thead>
         <tbody>
           {data.filter((data) => {
-            console.log(data.name.toLowerCase(), 'ki')
+            console.log(data, 'ki')
             if (data.name.toLowerCase().includes(query)) {
               return data
             }
@@ -58,12 +73,19 @@ function DetailsTable() {
               <td>
                 <input type="checkbox" />
               </td>
-              <td>{item.name}</td>
+              <td>
+                {isEditing ? <input
+                  type='text'
+                  placeholder='e.g. amnah'
+                  value={item.name}
+                  onChange={(e) => setName(e.target.value)}
+                /> : item.name}
+              </td>
               <td>{item.email}</td>
               <td>{item.role}</td>
               <td>
                 <FaEdit />
-                <FaTrash />
+                <FaTrash onClick={() => deleteItem(item.id)} />
               </td>
             </tr>
           ))
@@ -74,9 +96,103 @@ function DetailsTable() {
   )
 }
 
+
+
+
 const Container = styled.div`
-.th{
-  background-color:red;
+table {
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  table-layout: fixed;
+}
+
+table caption {
+  font-size: 1.5em;
+  margin: .5em 0 .75em;
+}
+
+table tr {
+  background-color: #f8f8f8;
+  border: 1px solid #ddd;
+  padding: .35em;
+}
+
+table th,
+table td {
+  padding: .625em;
+  text-align: center;
+}
+
+table th {
+  font-size: .85em;
+  letter-spacing: .1em;
+  text-transform: uppercase;
+}
+
+@media screen and (max-width: 600px) {
+  table {
+    border: 0;
+  }
+
+  table caption {
+    font-size: 1.3em;
+  }
+  
+  table thead {
+    border: none;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+  
+  table tr {
+    border-bottom: 3px solid #ddd;
+    display: block;
+    margin-bottom: .625em;
+  }
+  
+  table td {
+    border-bottom: 1px solid #ddd;
+    display: block;
+    font-size: .8em;
+    text-align: right;
+  }
+  
+  table td::before {
+    /*
+    * aria-label has no advantage, it won't be read inside a table
+    content: attr(aria-label);
+    */
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  
+  table td:last-child {
+    border-bottom: 0;
+  }
 }
 `
 export default DetailsTable;
+
+// if (!root) return 0;
+// let diameter = 0;
+// height(root);
+// return diameter;
+
+// function height(node) {//4,2,3,1
+//   if (!node) return 0; //0,0,0,0,0,0
+
+//   const leftHeight = height(node.left);//2,4,null,null,null,2
+//   const rightHeight = height(node.right);//null,5,null,3,null,1
+//   diameter = Math.max(diameter, leftHeight + rightHeight)//0,0,2,2,3
+//   return Math.max(leftHeight, rightHeight) + 1 //1,1,2,1,3;
+// }
